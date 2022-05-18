2acd353ec54ed633124a9face4f4455d6115462a
Alex macbookAir <aleksandrmisnov@gmail.com>
2022-05-18 23:04:49 +0100

    started user routes

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,13 @@ const dotenv = require('dotenv').config()
 const PORT = process.env.PORT || 4000
 
 const app = express();
+const version = 'v1'
+
+app.get(`/`, (req, res) => {
+  res.status(200).json({message : 'Welcome to Support System API'})
+})
+// User Routes
+app.use(`/api/${version}/users`, require('./routes/userRoutes'))
 
 app.listen(PORT, () => console.log("Server running on port " + PORT));
 
