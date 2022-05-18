a5a394ebb8f5804ab4ec568b3d8f669065e2b305
Alex macbookAir <aleksandrmisnov@gmail.com>
2022-05-18 16:48:11 +0100

    continuing setup

---
+++ backend/server.js
@@ -0,0 +1,9 @@
+const express = require('express')
+const dotenv = require('dotenv').config()
+const PORT = process.env.PORT || 4000
+
+const app = express();
+
+app.listen(PORT, () => console.log("Server running on port " + PORT));
+
+console.log("Server started");
