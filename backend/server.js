6bdc499975194079dae1f346802d53891250bf1a
Alex macbookAir <aleksandrmisnov@gmail.com>
2022-05-19 15:30:38 +0100

    added db.js, mongoose connection to atlas

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,18 @@
 const express = require('express')
 const dotenv = require('dotenv').config()
-const PORT = process.env.PORT || 4000
+const colors = require('colors')
+
 
 const {errorHandler} = require('./middleware/errorMiddleware')
-
+const {connectDB} = require('./config/db')
 const app = express();
 app.use(express.json())
 app.use(express.urlencoded({extended: false}))
+
+const PORT = process.env.PORT || 4000
+
+//Connect to database
+connectDB() 
 
 
 const version = 'v1'
