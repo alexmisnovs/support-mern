15c055cbc48e4b3a6d08d90847178cd7b5a43f84
Alex macbookAir <aleksandrmisnov@gmail.com>
2022-05-19 15:42:05 +0100

    added userModel, finished register user function

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,4 +1,6 @@
 const asyncHandler = require('express-async-handler')
+const bcrypt = require('bcryptjs')
+const User = require('../models/userModel')
 // @desc Register new user
 // @route /api/v1/users
 // @access Public 
@@ -11,8 +13,35 @@ const registerUser = asyncHandler( async (req, res) => {
     res.status(400)
     throw new Error('Please include all fields')
   }
-  console.log(req.body)
-  res.json(req.body)
+  // find if user already exists
+  const userExists = await User.findOne({email})
+
+  if(userExists) {
+    res.status(400)
+    throw new Error('User already exists')
+  }
+
+  // hash password
+  const salt = await bcrypt.genSalt(10)
+  const hashedPassword = await bcrypt.hash(password, salt)
+
+  // create user
+  const user = await User.create({
+    name, email, password: hashedPassword
+  })
+
+  if(user) {
+    res.status(201).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email
+    })
+  } else{
+    res.status(400)
+    throw new Error('Invalid user data provided')
+  }
+  // console.log(req.body)
+  // res.json(req.body)
 
 })
 // @desc Login user
