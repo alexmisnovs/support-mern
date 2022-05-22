c4a0a25399f1f40d16fa77b9aca7cbe8a50c5168
Alex macbookAir <aleksandrmisnov@gmail.com>
2022-05-22 22:48:06 +0100

    added contact page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Contact from "./pages/Contact";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
@@ -11,6 +12,7 @@ function App() {
             <Route path="/" element={<Home />} />
             <Route path="/login" element={<Login />} />
             <Route path="/register" element={<Register />} />
+            <Route path="/contact-us" element={<Contact />} />
           </Routes>
         </div>
       </Router>
