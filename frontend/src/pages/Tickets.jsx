47d7d60d3535a0aa1cbb8b6c9951da7944dcf1ad
Alex macbookAir <aleksandrmisnov@gmail.com>
2022-06-01 09:28:52 +0100

    added TicketItem and display of tickets for a user

--- frontend/src/pages/Tickets.jsx
+++ frontend/src/pages/Tickets.jsx
@@ -6,19 +6,19 @@ import { getTickets, reset } from "../features/tickets/ticketSlice";
 
 import Spinner from "../components/Spinner";
 import { BackButton } from "../components/BackButton";
+import TicketItem from "../components/TicketItem";
 
 const Tickets = () => {
-  const { user } = useSelector(state => state.auth);
   const { isLoading, isError, isSuccess, message, tickets } = useSelector(state => state.ticket);
   const dispatch = useDispatch();
 
-  // useEffect(() => {
-  //   return () => {
-  //     if (isSuccess) {
-  //       dispatch(reset());
-  //     }
-  //   };
-  // }, [dispatch, isSuccess]);
+  useEffect(() => {
+    return () => {
+      if (isSuccess) {
+        dispatch(reset());
+      }
+    };
+  }, [dispatch, isSuccess]);
 
   useEffect(() => {
     dispatch(getTickets());
@@ -30,7 +30,17 @@ const Tickets = () => {
   return (
     <>
       <BackButton url="/" />
-      Tickets
+      <h1>Tickets</h1>
+      <div className="tickets">
+        <div className="ticket-headings">
+          <div>Date</div>
+          <div>Product</div>
+          <div>Status</div>
+        </div>
+        {tickets.map(ticket => (
+          <TicketItem key={ticket.id} ticket={ticket} />
+        ))}
+      </div>
     </>
   );
 };
